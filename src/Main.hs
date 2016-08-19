4eaab70d4c3ab0af6f70a8b55fa116d9791d568a
Jonathan Windridge <jonathan.windridge@trium-capital.com>
2016-08-19 16:28:18 +0100

    Add solution to problem 6

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,7 @@ import qualified Sols.Prob2
 import qualified Sols.Prob3
 import qualified Sols.Prob4
 import qualified Sols.Prob5
+import qualified Sols.Prob6
 
 solveProblem :: String -> String
 solveProblem strProblemNumber
@@ -15,6 +16,7 @@ solveProblem strProblemNumber
   | problemNumber == 3 = Sols.Prob3.solution
   | problemNumber == 4 = Sols.Prob4.solution
   | problemNumber == 5 = Sols.Prob5.solution
+  | problemNumber == 6 = Sols.Prob6.solution
   | otherwise          = "Not solved yet"
   where problemNumber = read strProblemNumber :: Int
 
---
+++ src/Sols/Prob6.hs
@@ -0,0 +1,12 @@
+module Sols.Prob6 (
+  solution
+) where
+  sumOfSquares :: [Integer] -> Integer
+  sumOfSquares x = sum (map (^2) x)
+
+  squareOfSum :: [Integer] -> Integer
+  squareOfSum x = sum x ^ 2
+
+  inputRange = [1..100]
+
+  solution = show (abs(sumOfSquares inputRange - squareOfSum inputRange))
